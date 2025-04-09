9ccbae3299e396df579b7849b249de0260866d3a
El-dorado01 <doctor.eldorado@gmail.com>
2025-04-09 21:50:54 +0100

    Add PHP 8.3 binary to functions for Netlify...

--- functions/api.js
+++ functions/api.js
@@ -1,19 +1,26 @@
 const { exec } = require('child_process');
+const path = require('path');
 
 exports.handler = async (event) => {
-    return new Promise((resolve) => {
-        exec(`php -f ${__dirname}/../public/index.php`, { env: { ...process.env, REQUEST_URI: event.path } }, (error, stdout, stderr) => {
-            if (error) {
-                resolve({
-                    statusCode: 500,
-                    body: JSON.stringify({ error: stderr }),
-                });
-            } else {
-                resolve({
-                    statusCode: 200,
-                    body: stdout,
-                });
-            }
-        });
-    });
+  return new Promise((resolve) => {
+    const phpPath = path.join(__dirname, 'php'); // Use bundled PHP
+    const indexPath = path.join(__dirname, '../public/index.php');
+    exec(
+      `${phpPath} -f ${indexPath}`,
+      { env: { ...process.env, REQUEST_URI: event.path } },
+      (error, stdout, stderr) => {
+        if (error) {
+          resolve({
+            statusCode: 500,
+            body: JSON.stringify({ error: stderr || error.message }),
+          });
+        } else {
+          resolve({
+            statusCode: 200,
+            body: stdout,
+          });
+        }
+      }
+    );
+  });
 };
