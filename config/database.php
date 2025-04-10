9183fdc87677dda1a6209d083b6b6624b35e73ce
El-dorado01 <102257373+El-dorado01@users.noreply.github.com>
2025-04-10 02:01:03 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => '/usr/local/share/ca-certificates/tidb-root.crt',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]) : [],
         ],
 
